b2151591e5b7d3d8bea12f7ecb4a9acdf501c1fa
Ana <116079787+Ana-Diaz-A@users.noreply.github.com>
2024-02-16 08:47:07 +0100

    Update script.js

--- script.js
+++ script.js
@@ -11,10 +11,10 @@ const animated_bg_texts = document.querySelectorAll('.animated-bg-text')
 setTimeout(getData, 2500)
 
 function getData() {
-  header.innerHTML = '<img src="/img/banner.png" alt="" />'
+  header.innerHTML = '<img src="img/banner.png" alt="" />'
   title.innerHTML = 'Lorem ipsum'
   excerpt.innerHTML = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam posuere venenatis nisl et volutpat.'
-  profile_img.innerHTML = '<img src="/img/profile.png" alt="" />'
+  profile_img.innerHTML = '<img src="img/profile.png" alt="" />'
   name.innerHTML = 'Ana D.A.'
   date.innerHTML = 'Jul 19, 1982'
 
